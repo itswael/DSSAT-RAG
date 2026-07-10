2a1cb72007f9c7e0e56a33e53a6dbbce9dd9769f
Mohammad Wael <errwael@gmail.com>
2026-07-10 11:54:48 -0400

    feat: added initializer

--- backend/app/mappers/__init__.py
+++ backend/app/mappers/__init__.py
@@ -1,1 +1,16 @@
 """Mappers package initialization."""
+from app.mappers.simulation import (
+    map_canonical_to_simulation,
+    map_canonical_to_simulations,
+    map_canonical_outputs,
+    map_canonical_outputs_bulk,
+    map_simulation_to_response,
+)
+
+__all__ = [
+    "map_canonical_to_simulation",
+    "map_canonical_to_simulations",
+    "map_canonical_outputs",
+    "map_canonical_outputs_bulk",
+    "map_simulation_to_response",
+]
